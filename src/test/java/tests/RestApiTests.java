09d422132e87bf7320c9f2230f2da7fae04ae274
cheshi-mantu <cheshi.mantu@gmail.com>
2020-07-06 23:11:24 +0300

    initial tests with REST assured

--- src/test/java/tests/RestApiTests.java
+++ src/test/java/tests/RestApiTests.java
@@ -1,31 +1,72 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.*;
 
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
-
-import static helpers.Environment.*;
 import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Epic("QA.GURU QA automation course")
-@Story("Selenide TKB tests homeworx")
-@Tag("tkb_tests")
+@Feature("Work with REST API")
+@Story("REST API tests with REST Assured")
+@Tag("rest_api_tests")
 class RestApiTests extends TestBase {
-
+    public String startUrl = "https://reqres.in";
     @Test
-    @Description("Open page, find Вклады button, select closest A, then click")
-    @DisplayName("Open main page, click on Вклады button by closest A")
-    void pageOpenButtonClickClosestA() {
-        step ("Open Tinkoff main page", () -> open(url));
-        step("Locate and press Вклады on page top", () -> {
-            $(byText("Вклады")).closest("a").click();
-            $("h1").shouldHave(text("Откройте вклад"));
+    void simpleGetRestAssured() {
+        String usersList = get(startUrl + "/api/users?page=2").asString();
+        System.out.println(usersList);
+    }
+    @Test
+    void simpleApiGetJUnitAssertTrue() {
+        String usersList = get(startUrl + "/api/users?page=2").asString();
+        assertTrue(usersList.length() > 0, "usersList length: " +
+                usersList.length() + " with data: " + usersList);
+    }
+    @Test
+    void simpleApiGetHamcrestAssertThat() {
+        String usersList = get(startUrl + "/api/users?page=2").asString();
+        assertThat(usersList.length(), is(not(nullValue())));
+    }
+    @Test
+    void restAssuredNormalget() {
+        RestAssured.baseURI = "https://reqres.in";
+        RequestSpecification httpRequest = given();
+        Response response = httpRequest.request(Method.GET, "/api/users?page=2");
+        System.out.println(response.getBody().asString());
+    }
+    @Test
+    void parseJsonFromApiGetSimplified() {
+        RestAssured.baseURI = "https://reqres.in";
+        get("/api/users?page=2")
+                .then()
+                .body("total", is(12));
+    }
+    @Test
+    void parseJsonFromApiGet() {
+        RestAssured.baseURI = "https://reqres.in";
+        step("hello", ()-> {
+            Integer resultsTotal = given()
+                    .when()
+                    .get("/api/users?page=2")
+                    .then()
+                    .statusCode(200)
+                    .extract()
+                    .response()
+                    .path("total");
+            assertThat(resultsTotal, is(12));
+
         });
     }
 
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -14,22 +14,21 @@ import static helpers.Environment.isVideoOn;
 
 public class TestBase {
 
-    @BeforeAll
-    public static void setUp() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
-        configureSelenide();
-    }
-    @BeforeEach
-    public void BeforeEachTest(){
-        Configuration.startMaximized = true;
-    }
-    @AfterEach
-    public void afterEach(){
-        String sessionId = getSessionId();
-        attachScreenshot("Last screenshot");
-        attachPageSource();
-        attachAsText("Browser console logs", getConsoleLogs());
-        closeWebDriver();
-        if (isVideoOn) attachVideo(sessionId);
-    }
+//    @BeforeAll
+//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+//        configureSelenide();
+
+//    @BeforeEach
+//    public void BeforeEachTest(){
+//        Configuration.startMaximized = true;
+//    }
+//    @AfterEach
+//    public void afterEach(){
+//        String sessionId = getSessionId();
+//        attachScreenshot("Last screenshot");
+//        attachPageSource();
+//        attachAsText("Browser console logs", getConsoleLogs());
+//        closeWebDriver();
+//        if (isVideoOn) attachVideo(sessionId);
+//    }
 }
