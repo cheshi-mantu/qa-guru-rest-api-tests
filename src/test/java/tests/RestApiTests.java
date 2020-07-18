b99101b616e39dd0cb50016f0dbe17efa2d2ee74
cheshi-mantu <cheshi.mantu@gmail.com>
2020-07-18 18:07:34 +0300

    refactoring refining

--- src/test/java/tests/RestApiTests.java
+++ src/test/java/tests/RestApiTests.java
@@ -1,9 +1,9 @@
 package tests;
 
-import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
@@ -22,42 +22,47 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @Story("REST API tests with REST Assured")
 @Tag("rest_api_tests")
 class RestApiTests extends TestBase {
-    public String startUrl = "https://reqres.in";
+    public String baseUrl = "https://reqres.in";
+    Integer resultsTotal;
+
     @Test
     void simpleGetRestAssured() {
-        String usersList = get(startUrl + "/api/users?page=2").asString();
+        String usersList = get(baseUrl + "/api/users?page=2").asString();
         System.out.println(usersList);
     }
     @Test
     void simpleApiGetJUnitAssertTrue() {
-        String usersList = get(startUrl + "/api/users?page=2").asString();
+        String usersList = get(baseUrl + "/api/users?page=2").asString();
         assertTrue(usersList.length() > 0, "usersList length: " +
                 usersList.length() + " with data: " + usersList);
     }
     @Test
     void simpleApiGetHamcrestAssertThat() {
-        String usersList = get(startUrl + "/api/users?page=2").asString();
+        String usersList = get(baseUrl + "/api/users?page=2").asString();
         assertThat(usersList.length(), is(not(nullValue())));
     }
     @Test
     void restAssuredNormalget() {
-        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.baseURI = baseUrl;
         RequestSpecification httpRequest = given();
         Response response = httpRequest.request(Method.GET, "/api/users?page=2");
         System.out.println(response.getBody().asString());
     }
     @Test
     void parseJsonFromApiGetSimplified() {
-        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.baseURI = baseUrl;
         get("/api/users?page=2")
                 .then()
                 .body("total", is(12));
     }
     @Test
     void parseJsonFromApiGetHamcrestAssert() {
-        RestAssured.baseURI = "https://reqres.in";
-        step("hello", ()-> {
-            Integer resultsTotal = given()
+        step("Initialize baseURI = " + baseUrl, ()->{
+            RestAssured.baseURI = baseUrl;
+        });
+        step("Assign the results of REST API resuest to resultsTotal from JSON var total", ()-> {
+            resultsTotal = given()
+                    .filter(new AllureRestAssured())
                     .when()
                     .get("/api/users?page=2")
                     .then()
@@ -71,9 +76,23 @@ class RestApiTests extends TestBase {
     }
     @Test
     void parseJsonFromApiGetRestAssrdOnly() {
-        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.baseURI = baseUrl;
         step("hello", ()-> {
                 given()
+                .filter(new AllureRestAssured())
+                .when()
+                .get("/api/users?page=2")
+                .then()
+                .statusCode(200)
+                .body("total", is(12));
+        });
+    }
+    @Test
+    void parseJsonParseJsonDeeper() {
+        RestAssured.baseURI = baseUrl;
+        step("hello", ()-> {
+                given()
+                .filter(new AllureRestAssured())
                 .when()
                 .get("/api/users?page=2")
                 .then()
