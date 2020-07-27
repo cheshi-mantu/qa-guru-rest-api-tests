50de91aca8ccf63c8be31f8ae86c4a8bc337cf9a
cheshi-mantu <cheshi.mantu@gmail.com>
2020-07-27 21:54:38 +0300

    added tests 11 - 14 descriptions @DisplayName attachments to the tests where it is possible

--- src/test/java/tests/RestApiTests.java
+++ src/test/java/tests/RestApiTests.java
@@ -7,14 +7,16 @@ import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
+import io.restassured.config.HttpClientConfig;
+import io.restassured.config.RestAssuredConfig;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.apache.http.params.CoreConnectionPNames;
 import org.junit.jupiter.api.*;
 
 import static io.qameta.allure.Allure.step;
-import static io.restassured.RestAssured.get;
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -30,46 +32,60 @@ class RestApiTests extends TestBase {
     String email;
     String returnedValue;
     String token;
+    String errorText;
+    Response response;
+    RestAssuredConfig config;
 
     @Test
+    @DisplayName("1. Simple get, no assertions. Server response attached.")
     @Description("1. Simple RestAssured get request and sending it to System.out. No assertion.")
     void simpleGetRestAssured() {
         step("get by RestAssured, print to the std system output", ()-> {
             usersList = get(baseUrl + "/api/users?page=2").asString();
+            AttachmentsHelper.attachAsText("Server response: ", usersList);
             System.out.println(usersList);
         });
     }
 
     @Test
+    @DisplayName("2. Assertion of length of response. Must be greater than zero.")
     @Description("2. Simple RestAssured with assert by JUnit5 means usersList length > 0")
     void simpleApiGetJUnitAssertTrue() {
         step("get with rest assured, assert by JUnit5 with comments", ()-> {
             usersList = get(baseUrl + "/api/users?page=2").asString();
             assertTrue(usersList.length() > 0, "usersList length: " +
                     usersList.length() + " with data: " + usersList);
+            AttachmentsHelper.attachAsText("Server response: ", usersList);
         });
     }
 
     @Test
+    @DisplayName("3. Using hamcrest assertThat to assert response size.")
     @Description("3. Simple RestAssured get request assertion by means of hamcrest assertThat ans \'is\'")
     void simpleApiGetHamcrestAssertThat() {
         step("set usersList as the result of the get response by RestAssured" +
                 "check usersList length is not null (hamcrest)", ()-> {
             usersList = get(baseUrl + "/api/users?page=2").asString();
             assertThat(usersList.length(), is(not(nullValue())));
+            AttachmentsHelper.attachAsText("Server response: ", usersList);
         });
     }
 
     @Test
+    @DisplayName("3. Using hamcrest assertThat to assert response size. Different way to build request.")
     @Description("3.1 Simple RestAssured get request no assertion just different way to build the request")
     void restAssuredNormalget() {
-        RestAssured.baseURI = baseUrl;
-        RequestSpecification httpRequest = given();
-        Response response = httpRequest.request(Method.GET, "/api/users?page=2");
-        System.out.println(response.getBody().asString());
+        step("Build request. Gert response. No assertion. Attach response", ()->{
+            RestAssured.baseURI = baseUrl;
+            RequestSpecification httpRequest = given();
+            Response response = httpRequest.request(Method.GET, "/api/users?page=2");
+            System.out.println(response.getBody().asString());
+            AttachmentsHelper.attachAsText("Server response: ", usersList);
+        });
     }
 
     @Test
+    @DisplayName("4. Using hamcrest \'is\' to assert response value.")
     @Description("4. Assert that returned result if 12 in one go, JSON parse by rest assured")
     void parseJsonFromApiGetSimplified() {
         RestAssured.baseURI = baseUrl;
@@ -82,6 +98,7 @@ class RestApiTests extends TestBase {
     }
 
     @Test
+    @DisplayName("5. Well formed rest-assured text. assert returned value")
     @Description("5. Std way to write rest-assured tests.")
     void parseJsonFromApiGetHamcrestAssert() {
         step("Initialize baseURI = " + baseUrl, ()->{
@@ -99,10 +116,11 @@ class RestApiTests extends TestBase {
                     .response()
                     .path("total");
             assertThat(resultsTotal, is(12));
-
+            AttachmentsHelper.attachAsText("Server response: ", String.valueOf(resultsTotal));
         });
     }
     @Test
+    @DisplayName("6. Well formed rest-assured text. assert value selected by JSON path")
     @Description("6. Variation of 5. Assertion by hamcrest's \'is\'")
     void parseJsonFromApiGetRestAssrdOnly() {
         RestAssured.baseURI = baseUrl;
@@ -115,9 +133,11 @@ class RestApiTests extends TestBase {
                 .then()
                 .statusCode(200)
                 .body("total", is(12));
+            AttachmentsHelper.attachAsText("Server response: ", usersList);
         });
     }
     @Test
+    @DisplayName("7. Parsing JSON response selecting deeper nodes")
     @Description("7. Deeper parsing of response. Parse response by index in array")
     void parseJsonParseJsonDeeper() {
         RestAssured.baseURI = baseUrl;
@@ -135,9 +155,11 @@ class RestApiTests extends TestBase {
         System.out.println("Email is;" + email);
         step("email should be tobias.funke@reqres.in", ()-> {
             assertThat(email, is("tobias.funke@reqres.in"));
+            AttachmentsHelper.attachAsText("Email is ", email);
         });
     }
     @Test
+    @DisplayName("8. Parsing JSON's get dictionary's value by key")
     @Description("8. Deeper parsing of response. Get response by key in dict")
     void parseJsonParseDictValues() {
         RestAssured.baseURI = baseUrl;
@@ -155,9 +177,11 @@ class RestApiTests extends TestBase {
         System.out.println("Response value from BE:" + returnedValue);
         step("Should be StatusCode Wekly", ()-> {
             assertThat(returnedValue, is("StatusCode Weekly"));
+            AttachmentsHelper.attachAsText("JSON Parsing result: ", returnedValue);
         });
     }
     @Test
+    @DisplayName("9. Single user get. Validate 1st name.")
     @Description("9. Single User data check")
     void parseJsonForSingleUser() {
         RestAssured.baseURI = baseUrl;
@@ -175,9 +199,11 @@ class RestApiTests extends TestBase {
         System.out.println("Response value from BE:" + returnedValue);
         step("Returned first name should be Janet", ()-> {
             assertThat(returnedValue, is("Janet"));
+            AttachmentsHelper.attachAsText("First name: ", returnedValue);
         });
     }
     @Test
+    @DisplayName("10. Requst missing user with id = 23. 404 to be returned")
     @Description("10. Non existing user data check. Expecting status 404 and null")
     void parseJsonUserDontExist() {
         RestAssured.baseURI = baseUrl;
@@ -199,6 +225,7 @@ class RestApiTests extends TestBase {
         });
     }
     @Test
+    @DisplayName("11. Simplified syntax to test missing user.")
     @Description("11. Non existing user data check. Expecting status 404 and null. Simplified syntax.")
     void parseJsonUserDontExistSimplified() {
         RestAssured.baseURI = baseUrl;
@@ -210,8 +237,9 @@ class RestApiTests extends TestBase {
         });
     }
     @Test
-    @Description("11. POST login request. Successful attempt.")
-    void postSuccessfullLoginRequest() {
+    @DisplayName("12. POST request for authentication by email and passowrd")
+    @Description("12. POST login request. Successful attempt. Check status code and token")
+    void postSuccessfulLoginRequest() {
 //        {
 //            "email": "eve.holt@reqres.in",
 //                "password": "cityslicka"
@@ -237,6 +265,74 @@ class RestApiTests extends TestBase {
             AttachmentsHelper.attachAsText("Returned token:", token);
             System.out.println("Returned token: " + token);
         });
+    }
+    @Test
+    @DisplayName("13. Unsuccessful attempt to login without password")
+    @Description("13. POST login request. Unsuccessful attempt du to password absence.")
+    void postUnsuccessfulLoginRequest() {
+//        {
+//            "email": "eve.holt@reqres.in",
+//                "password": "cityslicka" << will be missing in the request
+//        }
+        RestAssured.baseURI = baseUrl;
+        step("Preparing POST request to login to \'/api/login\' ", ()-> {
+            response =
+                given()
+                    .filter(new AllureRestAssured())
+                    .log().all()
+                    .contentType("application/json")
+                    .body("{\"email\": \"eve.holt@reqres.in\"}")
+                .when()
+                    .post("/api/login")
+                .then()
+                    .statusCode(400)
+                    .extract()
+                    .response();
+        });
+        step("ACT: set error to error from response", ()-> {
+            errorText = response
+                    .path("error");
+        });
+        step("CHECK: error string is \'Missing password\'", ()-> {
+            assertThat(errorText, is("Missing password"));
+            AttachmentsHelper.attachAsText("Response from server: ",
+                    response.statusCode() +" " + response.prettyPrint());
+        });
+
+    }
+    @Test
+    @DisplayName("14. Processing response timeouts")
+    @Description("14. POST login request. Unsuccessful attempt du to password absence.")
+    void restAssuredDelaysProcessing() {
+        step("PREP: setting baseURI", ()-> {
+            RestAssured.baseURI = baseUrl;
+        });
+        step("PREP rest assured config' ", ()-> {
+            config = RestAssured.config()
+                .httpClient(HttpClientConfig.httpClientConfig()
+                    .setParam(CoreConnectionPNames.CONNECTION_TIMEOUT, 5000)
+                    .setParam(CoreConnectionPNames.SO_TIMEOUT, 5000));
+        });
+        step("ACT: sending request ", ()-> {
+            response =
+                given()
+                    .filter(new AllureRestAssured())
+                    .config(config)
+                    .contentType("application/json")
+                    .when()
+                    .get(baseURI+"/api/users?delay=3")
+                    .then()
+                    .statusCode(200)
+                    .extract()
+                    .response();
+        });
+        step("CHECK: Checking the server response.", ()-> {
+            AttachmentsHelper.attachAsText("Response from server: ",
+                    response.statusCode() +" " + response.prettyPrint());
+            assertThat(response.statusCode(), is(equalTo(200)));
+        });
+
+
     }
 
 
