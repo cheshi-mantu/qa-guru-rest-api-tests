0db27e39febc40f39a20b9edf06b5f7f1c0b6059
Cheshi Mantu <cheshi.mantu@protonmail.com>
2020-10-03 10:08:32 +0300

    removed wrong dependencies

--- src/test/java/helpers/AttachmentsHelper.java
+++ src/test/java/helpers/AttachmentsHelper.java
@@ -1,8 +1,6 @@
 package helpers;
 
 import io.qameta.allure.Attachment;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 
 import java.net.MalformedURLException;
 import java.net.URL;
